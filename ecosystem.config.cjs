fa7e1a6ff15558596a65be194db8495d90474f11
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2026-07-27 18:11:44 -0300

    fix(deploy): enforce exec_mode fork in ecosystem.config.cjs for better-sqlite3 compatibility

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
       name: 'cucumbrain',
       script: 'src/index.js',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '300M',
